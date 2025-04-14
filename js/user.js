4100f0569f6bee348b5241517681f16b567aae75
nazmul-hossin <mdnazmulhossin999@gmail.com>
2025-04-14 16:56:53 +0600

    working on My Wallet

--- js/user.js
+++ js/user.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+// Sidebar toggle
     const $sidebar = $('#userSidebar');
 
     $('#openSidebarBtn').on('click', function () {
@@ -7,5 +8,14 @@ $(document).ready(function () {
 
     $('#closeSidebarBtn').on('click', function () {
         $sidebar.removeClass('show-sidebar');
+    });
+
+// Refresh button animation
+    $('.refresh-btn').on('click', function () {
+        const $icon = $(this).find('i');
+        $icon.addClass('fa-spin');
+        setTimeout(function () {
+            $icon.removeClass('fa-spin');
+        }, 1000);
     });
 });
